8f0472c78c0a76a609be750ed7e9f8d5103e54da
Eduard <etsidenkov@pureatmo.net>
2024-07-16 18:46:28 +0300

    fix: textarea

--- src/views/textarea.blade.php
+++ src/views/textarea.blade.php
@@ -2,12 +2,10 @@
     <label @if($id)for="{{$id}}"@endif>{{ $label }}</label>
     <div class="uk-form-controls">
         <textarea rows="{{ $rows }}"
-                  @if(!$class)class="uk-textarea" @endif
-                  @if($name)name="{{$name}}" @endif
-                  @if($id)id="{{$id}}"@endif
+            @if(!$class)class="uk-textarea" @endif
+            @if($name)name="{{$name}}" @endif
+            @if($id)id="{{$id}}"@endif
             {!! $attributes !!}
-        >
-            {{$value}}
-        </textarea>
+        >{{$value}}</textarea>
     </div>
 </div>
